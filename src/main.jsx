3df566b848f767a9e24b33cb466a7cc17acea18a
udaykalse <udaysinhk.2000@gmail.com>
2025-01-01 17:40:53 +0530

    ./

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+// import { useThemeStore } from "./store/useThemeStore.js";
 
-createRoot(document.getElementById('root')).render(
+// const storedTheme = localStorage.getItem("theme") || "light";
+// document.documentElement.setAttribute("data-theme", storedTheme);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
