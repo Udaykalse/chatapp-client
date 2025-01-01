9f071453e455217bc26bb72e306df6c4d0b8f736
udaykalse <udaysinhk.2000@gmail.com>
2025-01-01 18:27:35 +0530

    loader Ui design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,102 @@
-import React from 'react';
+// import React, { useEffect } from 'react';
+// import { Navbar } from './components/Navbar';
+// import {  Routes, Route, Navigate } from 'react-router-dom';
+// import  Home from './pages/Home';
+// import SignUpPage from './pages/SignUpPage';
+// import LoginPage from './pages/LoginPage';
+// import ProfilePage from './pages/ProfilePage';
+// import SettingsPage from './pages/SettingsPage';
+// import { useAuthStore } from './store/useAuthStore';
+// import {Loader} from 'lucide-react';
+
+// function App() {
+//   const {authUser,checkAuth, isCheckingAuth}=useAuthStore();
+//   useEffect(()=>{
+//     checkAuth();
+
+//   },[checkAuth]);
+
+//   console.log({authUser});
+//   if(isCheckingAuth && !authUser) return(
+//     <div className="flex items-center justify-center h-screen">
+//       <Loader className="size-10 animate-spin"/>
+//     </div>
+//   )
+//   return (
+//   <div>
+//   <Navbar/>
+//   <Routes>
+//     <Route path="/" element={authUser ? <Home />:<Navigate to="/login"/>} />
+//     <Route path="/signup" element={authUser ? <SignUpPage /> : <Navigate to="/"/>} />
+//     <Route path="/login" element={authUser ? <LoginPage /> : <Navigate to="/"/>} />
+//     <Route path="/settings" element={<SettingsPage /> } />
+//     <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login"/>  } />
+//   </Routes>
+//   </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const theme = useThemeStore();
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   return (
-  <div>
-    <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-  </div>
+    <div data-theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        {/* <Route path="/settings" element={authUser ? <SettingsPage /> : <Navigate to="/login" />} /> */}
+        <Route path="/settings" element={<SettingsPage />} />
+
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+
+        <Route path="*" element={<Navigate to={authUser ? "/" : "/login"} />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
--- src/components/skeletons/SidebarSkeleton.jsx
+++ src/components/skeletons/SidebarSkeleton.jsx
@@ -1,7 +1,6 @@
 import { Users } from "lucide-react";
 
 const SidebarSkeleton = () => {
-  // Create 8 skeleton items
   const skeletonContacts = Array(8).fill(null);
 
   return (
@@ -9,7 +8,6 @@ const SidebarSkeleton = () => {
       className="h-full w-20 lg:w-72 border-r border-base-300 
     flex flex-col transition-all duration-200"
     >
-      {/* Header */}
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="w-6 h-6" />
@@ -17,11 +15,9 @@ const SidebarSkeleton = () => {
         </div>
       </div>
 
-      {/* Skeleton Contacts */}
       <div className="overflow-y-auto w-full py-3">
         {skeletonContacts.map((_, idx) => (
           <div key={idx} className="w-full p-3 flex items-center gap-3">
-            {/* Avatar skeleton */}
             <div className="relative mx-auto lg:mx-0">
               <div className="skeleton size-12 rounded-full" />
             </div>
